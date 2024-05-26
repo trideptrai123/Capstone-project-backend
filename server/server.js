cf23e28681f1af449ea5922175d1962d1eb3b92a
Pham Phuoc Tri <tribestst123@gmail.com>
2024-05-26 12:29:32 +0700

    post check mail, username

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/capston-project');
+mongoose.connect('mongodb://localhost:27017/capston-project', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
@@ -30,6 +33,32 @@ app.post('/Signup', (req, res) => {
   User.create(req.body)
     .then((user) => res.json(user))
     .catch((err) => res.json(err));
+});
+
+app.post('/checkUsername', (req, res) => {
+  const { username } = req.body;
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        res.json({ exists: true });
+      } else {
+        res.json({ exists: false });
+      }
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+app.post('/checkEmail', (req, res) => {
+  const { email } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        res.json({ exists: true });
+      } else {
+        res.json({ exists: false });
+      }
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 app.listen(4000, () => {
