d59dee470ef86b1d07dd8b6ea1c21b6b53356792
Pham Phuoc Tri <tribestst123@gmail.com>
2024-05-28 10:31:36 +0700

    Create feature structure for be

--- server/models/User.js
+++ server/models/User.js
@@ -1,54 +1,22 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    role: {
-      type: String,
-      enum: ['university_student', 'high_school_student'],
-      required: true,
-    },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     isAdmin: {
       type: Boolean,
       required: true,
       default: false,
     },
+    userType: {
+      type: String,
+      required: true,
+      enum: ['Student university', 'Student highschool'], // Chỉ chấp nhận hai giá trị này
+    },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-// Hash the password before saving the user model
-// userSchema.pre('save', async function (next) {
-//   if (this.isModified('password') || this.isNew) {
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//   }
-//   next();
-// });
-
-// // Method to compare password
-// userSchema.methods.comparePassword = async function (password) {
-//   return bcrypt.compare(password, this.password);
-// };
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
--- server/server.js
+++ server/server.js
@@ -1,66 +1,38 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const router = require('./routes/router');
 const mongoose = require('mongoose');
-const User = require('./models/User');
+
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const authRoute = require('./routers/auth');
+const userRoute = require('./routers/user');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+dotenv.config();
+mongoose
+  .connect('mongodb://localhost:27017/capston-project', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Could not connect to MongoDB...', err));
 
-mongoose.connect('mongodb://localhost:27017/capston-project', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const corsOptions = {
+  origin: 'http://localhost:3000', // URL của frontend
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  User.findOne({ email: email }).then((user) => {
-    if (user) {
-      if (user.password === password) {
-        res.json('Success');
-      } else {
-        res.json('The password is incorrect');
-      }
-    } else {
-      res.json('No record existed');
-    }
-  });
-});
+//Routes
 
-app.post('/Signup', (req, res) => {
-  User.create(req.body)
-    .then((user) => res.json(user))
-    .catch((err) => res.json(err));
-});
+app.use('/v1/auth', authRoute);
+app.use('/v1/user', userRoute);
 
-app.post('/checkUsername', (req, res) => {
-  const { username } = req.body;
-  User.findOne({ username })
-    .then((user) => {
-      if (user) {
-        res.json({ exists: true });
-      } else {
-        res.json({ exists: false });
-      }
-    })
-    .catch((err) => res.status(500).json(err));
-});
-
-app.post('/checkEmail', (req, res) => {
-  const { email } = req.body;
-  User.findOne({ email })
-    .then((user) => {
-      if (user) {
-        res.json({ exists: true });
-      } else {
-        res.json({ exists: false });
-      }
-    })
-    .catch((err) => res.status(500).json(err));
-});
-
-app.listen(4000, () => {
-  console.log('server is running');
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
